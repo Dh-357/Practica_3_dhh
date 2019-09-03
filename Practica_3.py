629ca335148e8eec94ebc377eaae9fa8c95f8c06
Dh-357 <cripto357@ciencias.unam.mx>
2019-09-02 20:52:14 -0500

    ENTREGA

--- Practica_3.py
+++ Practica_3.py
@@ -1,57 +1,84 @@
+
+
 import os
 import itertools
+
+
 def mas_repetido(): #recibe matriz
-    limpia_pantalla()
-    matriz=input("ingrese_la_matriz entre corchetes [[n,a],[m,x]]:")
-    elemrep=[]
-    for x in matriz:
-        for i in x:
-            elemrep.append(i)
-    print  (max(set(elemrep),key=elemrep.count)) 
+    try:
+        limpia_pantalla()
+        matriz=input("ingrese_la_matriz entre corchetes [[n,a],[m,x]]:")
+        elemrep=[]
+        for x in matriz:
+            for i in x:
+                elemrep.append(i)
+        print  (max(set(elemrep),key=elemrep.count))
+    except:
+        print("Introduce bien tus datos")
+        raw_input("Re-iniciar funcion.....(press ENTER)")
+        mas_repetido()            
+
+    pass
       
 
 def condensa(): #recibe cadena
-    limpia_pantalla()
-    cadena=input("Introduce la cadena entre comillas:  ")
-    lista_palabras=list(cadena)
-    frecuencia_palabras=[]
-    for x in lista_palabras:
-        frecuencia_palabras.append(lista_palabras.count(x))
-    lista_repetida=list(zip(lista_palabras, frecuencia_palabras))
-    lista_nueva=[]
-    for y in lista_repetida:
-        if y not in lista_nueva:
-            lista_nueva.append(y)
-    print("Las parejas ordenadas con la letra y el numero de repeticiones es:\n")
-    print(lista_nueva)
+    try:
+        limpia_pantalla()
+        cadena=input("Introduce la cadena entre comillas:  ")
+        lista_palabras=list(cadena)
+        frecuencia_palabras=[]
+        for x in lista_palabras:
+            frecuencia_palabras.append(lista_palabras.count(x))
+        lista_repetida=list(zip(lista_palabras, frecuencia_palabras))
+        lista_nueva=[]
+        for y in lista_repetida:
+            if y not in lista_nueva:
+                lista_nueva.append(y)
+        print("Las parejas ordenadas con la letra y el numero de repeticiones es:\n")
+        print(lista_nueva)
+    except:
+        print("Introduce bien tus datos")
+        raw_input("Re-iniciar funcion.....(press ENTER)")
+        condensa()
+
     pass
 
 def triangulo_pascal(): #recibe niveles
-    limpia_pantalla()
-    nivel=int(input("Indique el nivel:   "))
-    if nivel<=0:
-        print "Introduce un numero mayor a 0:   " 
-    else:    
-        lista=[[1],[1,1]]
-        for i in range(1,nivel):
-            nivel = [1]
-            for j in range(0,len(lista[i])-1):
-                nivel.extend([lista[i][j] + lista[i][j+1]])
-            nivel+= [1]
-            lista.append(nivel)
-        print lista
+    try:
+        limpia_pantalla()
+        nivel=int(input("Indique el nivel:   "))
+        if nivel<=0:
+            print "Introduce un numero mayor a 0:   " 
+        else:    
+            lista=[[1],[1,1]]
+            for i in range(1,nivel):
+                nivel = [1]
+                for j in range(0,len(lista[i])-1):
+                    nivel.extend([lista[i][j] + lista[i][j+1]])
+                nivel+= [1]
+                lista.append(nivel)
+            print lista
+    except:
+        print("Introduce bien tus datos")
+        raw_input("Re-iniciar funcion.....(press ENTER)")
+        triangulo_pascal()
     pass
 
 def subcadenas(): #recibe cadena
-    limpia_pantalla()
-    listax=[]
-    listay=[]
-    cadena=input("Introduce la cadena (entre comillas):    ")
-    listax=list(cadena)
-    for L in range(0, len(listax)+1):
-        for elqsige in itertools.combinations(listax, L):
-            listay.append(elqsige)
-    print(listay)    
+    try:    
+        limpia_pantalla()
+        listax=[]
+        listay=[]
+        cadena=input("Introduce la cadena (entre comillas):    ")
+        listax=list(cadena)
+        for L in range(0, len(listax)+1):
+            for elqsige in itertools.combinations(listax, L):
+                listay.append(elqsige)
+        print(listay)
+    except:
+        print("Introduce bien tus datos")
+        raw_input("Re-iniciar funcion.....(press ENTER)")
+        subcadenas()    
     pass
 
 def limpia_pantalla():
@@ -69,43 +96,49 @@ def default():
 if __name__=="__main__":
     menu=True
     while menu:
-        eleccion=int(input("introduce el numero de funcion\n\n"+
-            "1.......Funcion(mas repetido)\n"+
-            "2.......Funcion(condensa)\n"+
-            "3.......Funcion(triangulo pascal)\n"+
-            "4.......Funcion(subcadenas)\n"+
-            "5.......SALIR DEL PROGRAMA\n\n"+
-            "input:  "))
-        if eleccion == 5:
-            menu=False
-            
-        elif eleccion == 1:
-            limpia_pantalla()
-            mas_repetido()
-            raw_input("Funcion completada.....(press ENTER)")
-            limpia_pantalla()
+        try:
+            eleccion=int(input("introduce el numero de funcion\n\n"+
+                "1.......Funcion(mas repetido)\n"+
+                "2.......Funcion(condensa)\n"+
+                "3.......Funcion(triangulo pascal)\n"+
+                "4.......Funcion(subcadenas)\n\n"+
+                "5.......SALIR DEL PROGRAMA\n\n"+
+                "input:  "))
     
+            if eleccion == 5:
+                menu=False
                 
-        elif eleccion == 2:
+            elif eleccion == 1:
+                limpia_pantalla()
+                mas_repetido()
+                raw_input("Funcion completada.....(press ENTER)")
+                limpia_pantalla()
+        
+                    
+            elif eleccion == 2:
+                limpia_pantalla()
+                condensa() 
+                raw_input("Funcion completada.....(press ENTER)")            
+                limpia_pantalla()
+            elif eleccion == 3:
+                limpia_pantalla()
+                triangulo_pascal()
+                raw_input("Funcion completada.....(press ENTER)")            
+                limpia_pantalla()
+            elif eleccion == 4:
+                limpia_pantalla()
+                subcadenas()
+                raw_input("Funcion completada.....(press ENTER)")            
+                limpia_pantalla()
+            else:
+                limpia_pantalla()
+                default()
+                raw_input("Regresar al Menu.....(press ENTER)")            
+                limpia_pantalla()
+        except:
             limpia_pantalla()
-            condensa() 
-            raw_input("Funcion completada.....(press ENTER)")            
-            limpia_pantalla()
-        elif eleccion == 3:
-            limpia_pantalla()
-            triangulo_pascal()
-            raw_input("Funcion completada.....(press ENTER)")            
-            limpia_pantalla()
-        elif eleccion == 4:
-            limpia_pantalla()
-            subcadenas()
-            raw_input("Funcion completada.....(press ENTER)")            
-            limpia_pantalla()
-        else:
-            limpia_pantalla()
-            default()
-            raw_input("Funcion completada.....(press ENTER)")            
-            limpia_pantalla()
-            
+            print("Introduce bien tus datos")
+            raw_input("Re-iniciar Menu.....(press ENTER)")
+        
         
     
